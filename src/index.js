875b19f2d1da05f8bc75b18f0a5feb676aa7291a
edgarrii <prodep007z@gmail.com>
2021-03-15 01:40:57 +0300

    fix: fix err2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  let arr = [];
+  let map = new Map();
+    
+  for (const iter of bracketsConfig) {
+    map.set(iter[0], iter[1]);
+  } 
+  arr.push(str[0]);
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === map.get(arr[arr.length - 1])) arr.pop(); 
+    else arr.push(str[i]);
+  }
+  if (arr.length > 0) return false;
+  return true;
 }
+  bracketsConfig = ['()', '{}', '12'];
