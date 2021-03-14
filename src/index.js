b028afaff096f4f55f4d6d39a9e38f63d9988a91
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-03-15 01:37:02 +0300

    feat: realize new functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  let arr = [];
+  let map = new Map();
+    
+  for (const iter of bracketsConfig) {
+    map.set(iter[0], iter[1]);
+  } 
+  array.push(str[0]);
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === map.get(array[array.length - 1])) array.pop(); 
+    array.push(str[i]);
+  }
+  if (array.length > 0) return false;
+  return true;
 }
