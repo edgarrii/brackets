dc9ebf1e518457cc4257613cbacc30cab3692883
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-03-15 01:38:14 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ module.exports = function check (str, bracketsConfig) {
   for (const iter of bracketsConfig) {
     map.set(iter[0], iter[1]);
   } 
-  array.push(str[0]);
+  arr.push(str[0]);
   for (let i = 1; i < str.length; i++) {
-    if (str[i] === map.get(array[array.length - 1])) array.pop(); 
-    array.push(str[i]);
+    if (str[i] === map.get(arr[arr.length - 1])) arr.pop(); 
+    arr.push(str[i]);
   }
-  if (array.length > 0) return false;
+  if (arr.length > 0) return false;
   return true;
 }
